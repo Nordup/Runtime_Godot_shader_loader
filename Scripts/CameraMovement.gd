f9e065424c5b3abb1555a1a95ce1fba6bafa4d1e
norka <nordup.ondr@gmail.com>
2021-04-25 16:09:38 +0300

    wheel scroll

--- Scripts/CameraMovement.gd
+++ Scripts/CameraMovement.gd
@@ -1,9 +1,12 @@
 extends Spatial
 
 const speed = -0.005
+const wheel_speed = 0.1
+onready var camera = get_child(0) as Spatial
+
 var rotate = false
 
-	
+
 func _unhandled_input(event):
 	if event is InputEventMouseMotion and rotate:
 		var movement = event.relative
@@ -12,7 +15,17 @@ func _unhandled_input(event):
 		# correct
 		rotation.x = clamp(rotation.x, deg2rad(-80), deg2rad(80))
 		rotation.z = 0
-		
+
+
+func _input(event):
+	if event is InputEventMouseButton and event.is_pressed():
+		if event.button_index == BUTTON_WHEEL_UP:
+			camera.translate(-camera.translation * wheel_speed)
+		elif event.button_index == BUTTON_WHEEL_DOWN:
+			camera.translate(camera.translation * wheel_speed)
+		camera.translation.z = clamp(camera.translation.z , 1.5, 100)
+
+
 func _process(delta):
 	if (Input.is_action_pressed("Click")):
 		rotate = true
